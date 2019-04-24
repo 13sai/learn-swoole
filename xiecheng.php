bbf8af7587f58645cf7b6cc0c214c1ffff4577f2
13sai <sai0556@qq.com>
2019-04-24 20:45:54 +0800

    协程

---
+++ xiecheng.php
@@ -0,0 +1,33 @@
+<?php
+
+//function test1()
+//{
+//    sleep(3);
+//    echo 'test1';
+//}
+//
+//function test2()
+//{
+//    sleep(2);
+//    echo 'test2';
+//}
+//
+//test1();
+//test2();
+
+
+//Swoole\Runtime::enableCoroutine();
+
+$t = time();
+
+go(function () use ($t){
+   sleep(6);
+   echo time() - $t;
+   echo 'a';
+});
+
+go(function () use ($t){
+   sleep(3);
+   echo time() - $t;
+   echo 'b';
+});
